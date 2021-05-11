66581e5be6f86ca50c5d6a32ed51c3866de3c056
Mr.Programmer <m.mohammadi.developer@gmail.com>
2021-05-11 22:39:42 +0430

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/admin', 'AdminsController@index')->name('admin.index');
+
